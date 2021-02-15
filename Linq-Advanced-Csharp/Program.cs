87fe94380377f46b838972e4975476cbe9c04dc7
Engin Karataş <enginkaratas99@gmail.com>
2021-02-15 21:30:07 +0300

    classes ready and using heap now
     new List<Product>()

--- Linq-Advanced-Csharp/Program.cs
+++ Linq-Advanced-Csharp/Program.cs
@@ -10,6 +10,27 @@ namespace Linq_Advanced_Csharp
     {
         static void Main(string[] args)
         {
+            List<Product> products = new List<Product>() {
+            new Product(){CategoryId=5,ProductName="maydanoz"},
+            new Product(){ProductName="mısır unu"}
+            };
+            Console.WriteLine(products[0].ProductName);
+            Console.Read();
+
         }
+    }
+    class Product
+    {
+        public int ProductId { get; set; }
+        public int CategoryId { get; set; }
+        public string ProductName { get; set; }
+        public string QuantityPerUnit { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int UnitsInStock { get; set; }
+    }
+    class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
     }
 }
