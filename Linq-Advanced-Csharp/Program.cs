3ecc2f7bae02e9a91c52a0aa1df9c695df481ace
Engin Karataş <enginkaratas99@gmail.com>
2021-02-15 21:20:01 +0300

    Initial commit

---
+++ Linq-Advanced-Csharp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq_Advanced_Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
