d2cbb98acfd5abf8e6051eb0f1dbe26bdf3294e8
Engin Karataş <enginkaratas99@gmail.com>
2021-02-15 22:15:21 +0300

    case1: diffrence between linq and non-linq query

--- Linq-Advanced-Csharp/Program.cs
+++ Linq-Advanced-Csharp/Program.cs
@@ -10,11 +10,66 @@ namespace Linq_Advanced_Csharp
     {
         static void Main(string[] args)
         {
-            List<Product> products = new List<Product>() {
-            new Product(){CategoryId=5,ProductName="maydanoz"},
-            new Product(){ProductName="mısır unu"}
+            List<Category> categories = new List<Category>
+            {
+                new Category{CategoryId=1,CategoryName="sebzeler"},
+                new Category{CategoryId=2,CategoryName="meyveler"}
             };
-            Console.WriteLine(products[0].ProductName);
+
+            List<Product> products = new List<Product>{
+            new Product{ProductId=1, CategoryId=1,ProductName="maydanoz"},
+            new Product{ProductId=2, CategoryId=1,ProductName="marul"},
+            new Product{ProductId=3, CategoryId=2,ProductName="domates"}
+            };
+
+            //call "maydanoz" and category id = 1 via every method and non-mathod 
+
+            //nolinq
+            foreach (var item in products)
+            {
+                if (item.ProductId == 1 && item.ProductName == "maydanoz")
+                {
+                    Console.WriteLine(item.ProductName);
+                }
+            }
+
+            //nolinql via method 
+            foreach (var item in GetProducts(products))
+            {
+                Console.WriteLine(item.ProductName);
+            }
+
+            List<Product> GetProducts(List<Product> productsParam)
+            {
+                List<Product> containerProducts = new List<Product>();
+                foreach (var item in productsParam)
+                {
+                    if (item.ProductId == 1 && item.ProductName == "maydanoz")
+                    {
+                        containerProducts.Add(item);
+                    }
+                }
+                return containerProducts;
+            }
+
+            ////linq       
+            var result = products.Where(p => p.ProductId == 1 && p.ProductName == "maydanoz");
+            foreach (var item in result)
+            {
+                Console.WriteLine(item.ProductName);
+            }
+
+            foreach (var item in GetProducts2(products))
+            {
+                Console.WriteLine(item.ProductName);
+            }
+
+            ////linq via method
+            List<Product> GetProducts2(List<Product> productsParam)
+            {
+                return productsParam.Where(p => p.ProductId == 1 && p.ProductName == "maydanoz").ToList();
+            }
+
             Console.Read();
 
         }
